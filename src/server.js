31f1ad5a142572f52868fee7a8dc2dad24bdbb3f
Barbara Bahamondez <barbara.b.m@outlook.cl>
2023-07-20 02:07:48 -0400

    add cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 import health from './router/health';
 import containers from './router/containers';
 
 const app = express();
 const port = 8000;
+
+app.use(cors())
 
 app.use('/api', health);
 app.use('/api/containers', containers);
